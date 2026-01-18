985adfab4fd9d71c2936fa5c48ee828970bdf995
Max Barrett <34328348+MaximillianGroup@users.noreply.github.com>
2026-01-17 21:47:16 -0800

    Update webpack config to support .mjs files

--- webpack.config.js
+++ webpack.config.js
@@ -6,26 +6,33 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = {
     mode: 'production',
     entry: {
-        // Main App
         'sparxstar-3iatlas-dictionary-app': './src/js/app.jsx',
-        // Independent Assets
         'sparxstar-3iatlas-dictionary-admin': './src/js/admin.js',
         'sparxstar-3iatlas-dictionary-form': './src/js/sparxstar-iatlas-dictionary-form.js',
-        'sparxstar-3iatlas-dictionary-admin-style': './src/css/admin.css', // Will be extracted
-        'sparxstar-3iatlas-dictionary-form-style':
-            './src/css/sparxstar-3iatlas-dictionary-form.css', // Will be extracted
+        'sparxstar-3iatlas-dictionary-admin-style': './src/css/admin.css',
+        'sparxstar-3iatlas-dictionary-form-style': './src/css/sparxstar-3iatlas-dictionary-form.css',
     },
     output: {
         path: path.resolve(__dirname, 'assets'),
         filename: 'js/[name].min.js',
         assetModuleFilename: 'images/[hash][ext][query]',
-        clean: false, // Don't clean whole assets folder, just overwrite
+        clean: false,
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        // FIX 1: Added '.mjs' to extensions list
+        extensions: ['.js', '.jsx', '.mjs', '.json', '.wasm'],
     },
     module: {
         rules: [
+            // FIX 2: Handle .mjs files explicitly for Apollo Client
+            {
+                test: /\.mjs$/,
+                include: /node_modules/,
+                type: 'javascript/auto',
+                resolve: {
+                    fullySpecified: false // Disable strict ESM imports
+                }
+            },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
@@ -41,7 +48,7 @@ module.exports = {
                 use: [
                     MiniCssExtractPlugin.loader,
                     'css-loader',
-                    'postcss-loader', // checks for postcss.config.js
+                    'postcss-loader',
                 ],
             },
             {
